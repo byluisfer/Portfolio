06d478035088675c81ebd0d002a16d3e785995cb
Luis Fernandez <luisferdass@gmail.com>
2024-10-15 15:40:20 +0200

    update education section

--- src/sections/EducationTimeline.jsx
+++ src/sections/EducationTimeline.jsx
@@ -9,11 +9,25 @@ const EducationTimeline = () => {
       {/* Elemento de trabajo */}
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "#0e0e11", color: "#ffffff" }}
-        contentArrowStyle={{ borderRight: "7px solid  #0e0e11" }}
+        contentStyle={{ background: "#17171B", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #17171B" }}
         date="August 23 - Aktualität"
-        iconStyle={{ background: "#1a285a", color: "#ffffff" }}
-        icon={<img src="/csbe.png" alt="Icon" style={{ width: '100%', height: '100%', borderRadius: '50%' }} /> }
+        iconStyle={{ background: "#fff" }}
+        icon={
+          <img 
+            src="/csbe.png" 
+            alt="Icon" 
+            style={{ 
+              width: '85%', 
+              height: '85%', 
+              borderRadius: '50%', 
+              position: 'absolute', 
+              top: '50%', 
+              left: '50%', 
+              transform: 'translate(-50%, -50%)' 
+            }} 
+          />
+        }
       >
         <h3 className="vertical-timeline-element-title">ICT Ausbildung</h3>
         <h4
@@ -21,66 +35,99 @@ const EducationTimeline = () => {
           style={{
             textShadow: `
               0 0 5px #9D3BE4,
-              0 0 10px #9D3BE4,
-              0 0 15px #9D3BE4
+              0 0 15px #9D3BE4,
+              0 0 30px #9D3BE4
             `,
           }}
         >
           Computer Schule Bern
         </h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project
-          Management, Team Leading
-        </p>
+        <ul class="mt-5 list-disc ml-5 space-y-2">
+          <li>Entwerfen und Erstellen von Webanwendungen mit HTML, CSS und JavaScript.</li>
+          <li>Collaborating effectively in team projects and independently solving tasks.</li>
+        </ul>
       </VerticalTimelineElement>
+
       <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        contentStyle={{ background: "#0e0e11", color: "#ffffff" }}
-        contentArrowStyle={{ borderRight: "7px solid  #0e0e11" }}
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "#17171B", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #17171B" }}
         date="August 21 - Juli 22"
-        iconStyle={{ background: "#9D3BE4", color: "#ffffff" }}
+        iconStyle={{ background: "#fff" }}
+        icon={
+          <img 
+            src="/morillon.png" 
+            alt="Icon" 
+            style={{ 
+              width: '85%', 
+              height: '85%', 
+              borderRadius: '50%', 
+              position: 'absolute', 
+              top: '50%', 
+              left: '50%', 
+              transform: 'translate(-50%, -50%)' 
+            }} 
+          />
+        }
       >
-        <h3 className="vertical-timeline-element-title">
-          Sekundarschule
-        </h3>
+        <h3 className="vertical-timeline-element-title">Sekundarschule</h3>
         <h4
           className="vertical-timeline-element-subtitle"
           style={{
             textShadow: `
               0 0 5px #9D3BE4,
-              0 0 10px #9D3BE4,
-              0 0 15px #9D3BE4
+              0 0 15px #9D3BE4,
+              0 0 30px #9D3BE4
             `,
           }}
         >
           Schulhaus Morillon
         </h4>
-        <p>Strategy, Social Media</p>
+        <ul class="mt-5 list-disc ml-5 space-y-2">
+          <li>Sich schnell an eine neue akademische Umgebung anpassen und gleichzeitig in einer Fremdsprache lernen.</li>
+          <li>Aufbau von Grundlagenwissen in verschiedenen Fächern und gleichzeitige Integration in die Schweizer Schulkultur.</li>
+        </ul>
       </VerticalTimelineElement>
 
       <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        contentStyle={{ background: "#0e0e11", color: "#ffffff" }}
-        contentArrowStyle={{ borderRight: "7px solid  #0e0e11" }}
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "#17171B", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #17171B" }}
         date="September 19 - Juli 20"
-        iconStyle={{ background: "#9D3BE4", color: "#ffffff" }}
+        iconStyle={{ background: "#fff" }}
+        icon={
+          <img 
+            src="/cristoRey.png" 
+            alt="Icon" 
+            style={{ 
+              width: '85%', 
+              height: '85%', 
+              borderRadius: '50%', 
+              position: 'absolute', 
+              top: '50%', 
+              left: '50%', 
+              transform: 'translate(-50%, -50%)' 
+            }} 
+          />
+        }
       >
-        <h3 className="vertical-timeline-element-title">
-          Sekundarschule
-        </h3>
+        <h3 className="vertical-timeline-element-title">Sekundarschule</h3>
         <h4
           className="vertical-timeline-element-subtitle"
           style={{
             textShadow: `
               0 0 5px #9D3BE4,
-              0 0 10px #9D3BE4,
-              0 0 15px #9D3BE4
+              0 0 15px #9D3BE4,
+              0 0 30px #9D3BE4
             `,
           }}
         >
           Colegio Cristo Rey
         </h4>
-        <p>Strategy, Social Media</p>
+        <ul class="mt-5 list-disc ml-5 space-y-2">
+          <li>Konsolidierung wichtiger akademischer Kenntnisse aus verschiedenen Fächern in einer dynamischen Lernumgebung.</li>
+          <li>Teilnahme an Gruppenprojekten, die Zusammenarbeit und Problemlösung fördern.</li>
+        </ul>
       </VerticalTimelineElement>
     </VerticalTimeline>
   );
