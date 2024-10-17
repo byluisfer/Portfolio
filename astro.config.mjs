4f8d59d57c7d49e323e8f20f7869a7620ade0668
Luis Fernandez <luisferdass@gmail.com>
2024-10-17 19:28:27 +0200

    better performance web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import compression from 'vite-plugin-compression';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: {
+    plugins: [
+      compression({
+        algorithm: 'brotliCompress', // O 'gzip'
+        ext: '.br',
+      }),
+    ],
+  },
 });
