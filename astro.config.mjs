04e3d8c73a7e311df174574e80c285a90ae1e0ab
Luis Fernandez <luisferdass@gmail.com>
2024-11-23 13:29:51 +0100

    Performance x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: {
+    build: {
+      minify: 'terser', // Usa Terser para una minificación avanzada
+      terserOptions: {
+        compress: {
+          drop_console: true, // Elimina console.log para ahorrar espacio
+          drop_debugger: true, // Elimina debugger
+          passes: 2, // Comprime el código dos veces para obtener un bundle más pequeño
+        },
+        format: {
+          comments: false, // Elimina comentarios en el código generado
+        },
+      },
+    },
+  },
+  build: {
+    minify: true, // Minifica los archivos generados (HTML, CSS, y JS)
+  },
 });
